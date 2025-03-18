8531b9a48ef111de58bf6e6a05fb8520dfd5564b
Hauharu <trunghauu71@gmail.com>
2025-03-18 10:41:31 +0700

    up

---
+++ Bai1_Buoi3_NguyenTrungHau_4/Bai1_Buoi3_NguyenTrungHau_4/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Bai1_Buoi3_NguyenTrungHau_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_hau_4 = new System.Windows.Forms.Button();
+            this.tbox_hau_4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_hau_4
+            // 
+            this.bt_hau_4.Location = new System.Drawing.Point(211, 268);
+            this.bt_hau_4.Name = "bt_hau_4";
+            this.bt_hau_4.Size = new System.Drawing.Size(186, 44);
+            this.bt_hau_4.TabIndex = 0;
+            this.bt_hau_4.Text = "OPEN BROWSER";
+            this.bt_hau_4.UseVisualStyleBackColor = true;
+            this.bt_hau_4.Click += new System.EventHandler(this.bt_hau_4_Click);
+            // 
+            // tbox_hau_4
+            // 
+            this.tbox_hau_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbox_hau_4.Location = new System.Drawing.Point(211, 154);
+            this.tbox_hau_4.Name = "tbox_hau_4";
+            this.tbox_hau_4.Size = new System.Drawing.Size(186, 29);
+            this.tbox_hau_4.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(458, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CLOSE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbox_hau_4);
+            this.Controls.Add(this.bt_hau_4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "url";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_hau_4;
+        private System.Windows.Forms.TextBox tbox_hau_4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Bai1_Buoi3_NguyenTrungHau_4/Bai1_Buoi3_NguyenTrungHau_4/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Bai1_Buoi3_NguyenTrungHau_4
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Đảm bảo khi khởi tạo không có lỗi màn hình đen (thiết lập ứng dụng không hiển thị màn hình đen)
+            Application.EnableVisualStyles();
+        }
+
+        private void bt_hau_4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Kiểm tra nếu textbox không rỗng
+                string url = string.IsNullOrEmpty(tbox_hau_4.Text.ToString()) ? "https://www.google.com" : tbox_hau_4.Text.ToString();
+
+                // Khởi tạo ChromeDriver
+                driver = new ChromeDriver();
+                driver.Url = url;
+
+                // Điều hướng đến trang web
+                driver.Navigate();
+
+                // Lấy URL hiện tại và hiển thị lên TextBox
+                string link_hientai = driver.Url;
+                tbox_hau_4.Text = link_hientai;  // Cập nhật URL vào TextBox
+
+                // Lấy Title và hiển thị lên TextBox hoặc Console nếu cần
+                string pageTitle = driver.Title;
+                Console.WriteLine(pageTitle);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi xảy ra: " + ex.Message);
+            }
+        }
+
+
+
+        // Đảm bảo không có màn hình đen khi ứng dụng chạy
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Đóng tất cả các cửa sổ liên kết
+                driver.Quit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Có lỗi khi đóng trình duyệt: " + ex.Message);
+            }
+        }
+    }
+}
